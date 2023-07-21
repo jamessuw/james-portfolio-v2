9a3aa663ecb3e8af80e03a683a0a7dfd3f35da55
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-07-21 21:40:13 +1000

    updating landing page, adding typography and mobile responsive

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,20 +1,56 @@
-import React from "react";
 import Spline from '@splinetool/react-spline';
+import React, { useState, useEffect } from "react";
 import './landing.css';
 
 
-function Landing() {
-  return (
-    <section>
-      <div className="container">
-        <div className="container-1">
-          <h1>Tesst</h1>
-          
-      
-<div className="binary-cube">
-<Spline scene="https://prod.spline.design/1YV3mver-qvdV2nK/scene.splinecode" />
 
- </div>
+
+
+
+function Landing() {
+const [text, setText] = useState("")
+const [fullText, setfullText] = useState("FRONT-END ENGINNIERS")
+const [index, setIndex] = useState(0)
+
+useEffect(() => {
+  if (index < fullText.length) {
+    setTimeout(() => {
+      setText(text + fullText [index])
+        setIndex(index + 1)
+    }, 60)
+    
+  }
+}, [index]);
+
+
+
+
+  return (
+    <section id='sct-landing-mobile'>
+      <div className="container" id='landing-container'>
+        <div className="container-1" id="landing-page">
+<div className="binary-cube">
+<Spline  id="cube-canva" scene="https://prod.spline.design/1YV3mver-qvdV2nK/scene.splinecode" />
+
+
+</div>
+
+<div className='container-text'>
+<div className="landing-text">
+<h1>{text}</h1>
+<h5>James Wijaya</h5><a href="#" title="LinkedIn"><i class="fa fa-linkedin fa-fw"></i> LinkedIn</a> | <a href="#" title="LinkedIn"><i class="fa fa-linkedin fa-fw"></i> GitHub</a>
+
+
+</div>
+<div className='spacing'>
+
+</div>
+
+<div className='landing-catalog'>
+  <div className='circle' id='catalog-circle'></div>
+<h1>HEY, WELCOME TO MY PORTFOLIO FEEL FREE TO BROWSE AROUND! </h1>
+</div>
+</div>
           
         </div>
       </div>

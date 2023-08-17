da469055ea4fafb3abc5b8725971bd4f50905308
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-08-17 15:39:40 +1000

    development project section, adding function array catagory project pill

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -4,113 +4,108 @@ import { Container } from 'reactstrap'
 // import Popup from 'reactjs-popup';
 
 
-
-
-
-
-
 function Project() {
-   
-// const [isOpen, setIsOpen] = useState (false);  
+  const [hoveredItem, setHoveredItem] = useState(null);
 
-// const togglePopup = () => {
-//     setIsOpen(!isOpen);
-//   }
-const [isHovered, setIsHovered] = useState(false);
-
-
-const handleMouseEnter = () => {
-    setIsHovered(true);
+  
+  const handleMouseEnter = (itemName) => {
+    setHoveredItem(itemName);
   };
 
   const handleMouseLeave = () => {
-    setIsHovered(false);
+    setHoveredItem(null);
   };
 
+  const projectItems = [
+    {
+      name: "KumalaJingga",
+      imageSrc: "kumalajingga.png",
+      text: "",
+      tech:"Wordpress",
+      service:"Web Development",
+      location:"Indonesia"
+    },
+    {
+      name:"ClearDynamics",
+      imageSrc:"CD.png",
+      text:"",
+      service:"Contract",
+      location:"Melbourne",
+      tech:"Professionals"
+  },
+    {
+      name: "Meetbowl",
+      imageSrc: "meetbowl.png",
+      text: "",
+      tech:"SquareSpace",
+      service:"Web Development",
+      location:"Melbourne"
+    },
+    {
+      name: "etome",
+      imageSrc: "etome.png",
+      text: "",
+      tech:"Shopify",
+      service:"Web Development",
+      location:"Indonesia"
+    },
+
+    // Add more project items here
+
+    
+  ];
 
   return (
+
+    
     <section id='project-section-container'>
-      <div className='project-head'> 
-            <h1>PROJECT</h1>
-            <h2>ARCHIVE</h2>
-        </div>  
-    <div className='container'>
-    
-    <div className='container-1' id='project-container'>
+      <h1>PROJECT</h1>
+      <h2>ARC`HIVE</h2>
+      <div className='container'>
+      {/* list container descriptions*/}
+      <div className='container-1'>
+
+        {projectItems.map((item) => (
+          <div
+            key={item.name}
+            className='project-container'
+            onMouseEnter={() => handleMouseEnter(item.name)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <div className='list-text'>
+              <h4>{item.name}</h4>   
+              <div className='project-pill-container'>
+              <div className='project-pill'> <p>{item.tech}</p></div><div className='project-pill'> <p>{item.location}</p></div><div className='project-pill'> <p>{item.service}</p></div>
+              </div>
+            </div>
+          </div>
+        ))}
         
-        <div className='container-image-project'>
-           
-            <div className='list-project'
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}>
-             <div className='list-text'><h4>KumalaJingga</h4>
-             </div>
-            <div className='project-pill-container'><div className='project-pill'><p>Indonesia</p></div><div className='project-pill'><p>Web Development</p></div><div className='project-pill'><p>WordPress</p></div><div className='project-pill'><p>SEO</p></div></div>
-            
-           </div>
-           <div className='list-project'>
-             <div className='list-text'><h4>Meetbowl</h4>
-             </div>
-            <div className='project-pill-container'><div className='project-pill'><p>Indonesia</p></div><div className='project-pill'><p>Web Development</p></div><div className='project-pill'><p>WordPress</p></div><div className='project-pill'><p>SEO</p></div></div>  
-           </div>
-
-           <div className='list-project'>
-             <div className='list-text'><h4>Etome</h4>
-             </div>
-            <div className='project-pill-container'><div className='project-pill'><p>Indonesia</p></div><div className='project-pill'><p>Web Development</p></div><div className='project-pill'><p>WordPress</p></div><div className='project-pill'><p>SEO</p></div></div>
-            
-             {/* <div className='img-project'> <img src="meetbowl.png" alt="" /></div> */}
-           </div>
-
-           <div className='list-project'>
-             <div className='list-text'><h4>Admin Dashboard</h4>
-             </div>
-            <div className='project-pill-container'><div className='project-pill'><p>Indonesia</p></div><div className='project-pill'><p>Web Development</p></div><div className='project-pill'><p>WordPress</p></div><div className='project-pill'><p>SEO</p></div></div>
-            
-             {/* <div className='img-project'> <img src="meetbowl.png" alt="" /></div> */}
-           </div>
-
-         
-        
-        
-</div>
-
-
-       
-
-{/* 
-
-    <Popup trigger=
-                { <button id="folder-button" onClick={togglePopup}>
-                <div class="folder">
-                  <div class="folder__back">
-                    <div class="paper"></div>
-                    <div class="paper"></div>
-                    <div class="paper"></div>
-                    <div class="paper"></div>
-                    <div class="folder__front"></div>
-                    <div class="folder__front right"></div>
-                  </div>
-                  <span class="project_name">
-                  <p>Meetbowl</p>
-                  </span>
-                  </div>
-                </button>}
-                position="top center">
-                <div className='window-pop'>GeeksforGeeks</div>
-               
-            </Popup>
- */}
-
-    </div>
-      <div className='container-1'></div>
-     {isHovered && <img className='img-project'src="kumalajingga.png" alt="Hovered Image" />}
       </div>
-    
 
+    {/* Image container -project */}
+      <div className='container-1'>
 
-
-
+        {projectItems.map((item) => (
+          <div
+            key={item.name}
+            className='image-container'
+          >
+            {hoveredItem === item.name && (
+              <div className='hovered-item'>
+                <img
+                  className='img-project'
+                  src={item.imageSrc}
+                  alt={item.name}
+                />
+                <p>{item.text}</p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      {/* ... */}</div>
+      
     </section>
 
 

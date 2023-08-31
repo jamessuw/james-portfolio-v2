96635d9fd72124e9e887c197d0c32589b23e59bf
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-09-01 00:34:19 +1000

    developing ScrollTrigger card section in App.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,65 @@ import Landing from './components/Landing/Landing';
 import About from './components/About/About';
 import Project from './components/Project/Project';
 import Contact from './components/Contact/Contact';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from 'react';
+
+
+
 
 
 
 function App() {
+  let sections = gsap.utils.toArray(".panel");
+
+  useEffect(() => {
+    const sections = gsap.utils.toArray('.panel');
+
+    ScrollTrigger.config({ 
+      ignoreMobileResize: true
+    });
+
+    sections.forEach((panel, i) => {
+      gsap.to(panel, {
+        scrollTrigger: {
+          trigger: panel,
+          start: 'top top',
+          scrub: 1,
+          makers:true,
+          pin: true,
+          pinSpacing: false,
+          end: "+=150%",
+          // scroller: '.container', // for mobile responsive 
+          snap: {
+            snapTo: 1,
+            duration: { min: 0.5, max: 0.5 }
+          }
+        }
+      });
+    });
+  }, []);
+
+  
+  ScrollTrigger.config({ 
+    ignoreMobileResize: true
+  });
   return (
     <>
     {/* <ParticleComp/> */}
     <NavMenu/>
+    <section class="panel">
     <Landing/>
-    <About/>
+    </section>
+    <section class="panel">
+     <About/>
+    </section>
+    <section className='panel'>
     <Project/>
+    </section>
+    <section class="panel">
     <Contact/>
-   
+   </section>
 </>
   );
 }
